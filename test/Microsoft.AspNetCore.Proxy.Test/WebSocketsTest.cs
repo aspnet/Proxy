019bc8dde29c722855fc86c21c52091a53ae90ab
Marcin Kosieradzki <m.kosieradzki@p2ware.com>
2017-02-24 03:02:44 +0100

    Added license header Renamed test method Code formatting Added constants

--- test/Microsoft.AspNetCore.Proxy.Test/WebSocketsTest.cs
+++ test/Microsoft.AspNetCore.Proxy.Test/WebSocketsTest.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.TestHost;
 using Xunit;
 
 namespace Microsoft.AspNetCore.Proxy.Test
@@ -24,50 +24,58 @@ namespace Microsoft.AspNetCore.Proxy.Test
                 Assert.Equal(WebSocketMessageType.Text, res.MessageType);
                 received += res.Count;
                 if (res.EndOfMessage)
+                {
                     return Encoding.UTF8.GetString(recvBuff, 0, received);
+                }
                 Assert.InRange(received, 0, maxLen);
             }
         }
 
         [Fact]
-        public async Task SmokeTest1()
+        public async Task ProxyWebSocketsSmokeTest()
         {
-            var server = new WebHostBuilder()
+            const string supportedSubProtocol = "myproto2";
+            const string otherSubProtocol = "myproto1";
+            const string message1Content = "TEST MESSAGE 1";
+            const string message2Content = "TEST MSG 2";
+            const string message3Content = "TEST MESSAGE 3";
+            const string closeStatusDescription = "My Status1";
+
+            using (var server = new WebHostBuilder()
                 .UseKestrel()
                 .Configure(app => app.UseWebSockets().Run(async ctx =>
                 {
-                    var socket = await ctx.WebSockets.AcceptWebSocketAsync("myproto2");
+                    var socket = await ctx.WebSockets.AcceptWebSocketAsync(supportedSubProtocol);
                     var message1 = await ReceiveTextMessage(socket);
                     var message2 = await ReceiveTextMessage(socket);
-                    Assert.Equal("TEST MESSAGE 1", message1);
-                    Assert.Equal("TEST MSG 2", message2);
-                    await socket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("TEST MESSAGE 3")), WebSocketMessageType.Text, true, CancellationToken.None);
-                    await socket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "My Status1", CancellationToken.None);
-                })).Start("http://localhost:4001");
-
-            var proxy = new WebHostBuilder()
+                    Assert.Equal(message1Content, message1);
+                    Assert.Equal(message2Content, message2);
+                    await socket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(message3Content)), WebSocketMessageType.Text, true, CancellationToken.None);
+                    await socket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, closeStatusDescription, CancellationToken.None);
+                })).Start("http://localhost:4001"))
+            using (var proxy = new WebHostBuilder()
                 .UseKestrel()
-                .Configure(app => app.UseWebSockets().RunProxy(new ProxyOptions { Scheme = "http", Host = "localhost", Port = "4001" })).Start("http://localhost:4002");
-
+                .Configure(app => app.UseWebSockets().RunProxy(new ProxyOptions { Scheme = "http", Host = "localhost", Port = "4001" }))
+                .Start("http://localhost:4002"))
             using (var client = new ClientWebSocket())
             {
-                client.Options.AddSubProtocol("myproto1");
-                client.Options.AddSubProtocol("myproto2");
+                client.Options.AddSubProtocol(otherSubProtocol);
+                client.Options.AddSubProtocol(supportedSubProtocol);
                 await client.ConnectAsync(new Uri("ws://localhost:4002"), CancellationToken.None);
-                Assert.Equal("myproto2", client.SubProtocol);
-                await client.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("TEST MESSAGE 1")), WebSocketMessageType.Text, true, CancellationToken.None);
-                await client.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("TEST MSG 2")), WebSocketMessageType.Text, true, CancellationToken.None);
+                Assert.Equal(supportedSubProtocol, client.SubProtocol);
+
+                await client.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(message1Content)), WebSocketMessageType.Text, true, CancellationToken.None);
+                await client.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(message2Content)), WebSocketMessageType.Text, true, CancellationToken.None);
                 var message3 = await ReceiveTextMessage(client);
-                Assert.Equal("TEST MESSAGE 3", message3);
+                Assert.Equal(message3Content, message3);
+
                 var recv = await client.ReceiveAsync(new ArraySegment<byte>(new byte[4096]), CancellationToken.None);
                 Assert.Equal(WebSocketMessageType.Close, recv.MessageType);
                 Assert.Equal(WebSocketCloseStatus.NormalClosure, recv.CloseStatus);
-                Assert.Equal("My Status1", recv.CloseStatusDescription);
-                await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "My Status2", CancellationToken.None);
-            }
+                Assert.Equal(closeStatusDescription, recv.CloseStatusDescription);
 
-            server.Dispose();
-            proxy.Dispose();
+                await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, closeStatusDescription, CancellationToken.None);
+            }
         }
     }
 }
